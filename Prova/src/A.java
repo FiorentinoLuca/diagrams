5aedac48a1c9fa04fe86a6611fc940ecd31ba173
lfior <lfior@LucaPC.mshome.net>
2025-06-11 10:21:40 +0200

    first commit

---
+++ Prova/src/A.java
@@ -0,0 +1,8 @@
+
+public interface A {
+
+	void f1();
+	void f2();
+	void f3();
+	
+}
---
+++ Prova/src/B.java
@@ -0,0 +1,13 @@
+
+public abstract class B implements A {
+	
+	@Override
+	public void f1() {
+		System.out.println("f1 di B");
+	}
+	
+	default void g() {
+		System.out.println("f2 di B");
+	}
+
+}
